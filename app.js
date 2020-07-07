313da16b3ba0c7c3e6064ecf4da6f6c03646bc29
sukhleen3005 <sukhleen.kaur@jungleoworks.com>
2020-07-07 19:37:29 +0530

    updated

--- app.js
+++ app.js
@@ -3,27 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var bodyParser = require('body-parser')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var homeRouter = require('./routes/home')
 
-// var pool = require('./database')()
-var hb = require('./database')()
 var app = express();
+// app.use(bodyParser);
 
-app.get('/', (req, res) => {
-   
-  hb.query("SELECT * FROM mysampletable", (err, rows, fields) => {
-    if(err){
-      console.log('ERROR IN DA HOUSE', err)
-    }else{
-      console.log("sucessfull bitch", rows)
-    }
-  })
-})
-
-app.listen(1337)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -36,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/home', homeRouter)
+app.use('/customers', homeRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,58 @@
 var express = require("express");
 var router = express.Router();
+var hb = require('../database')()
 /* GET users listing. */
-const crypto = require('crypto')
+const crypto = require("crypto");
 
-router.get('/time', (req, res, next) => {
-    res.send({time: new Date().toISOString()
-    })
+
+//usual CRUD routine...
+
+router.get('/', (req, res) => {
+  hb.query("SELECT * FROM mysampletable", (err, rows, fields) => {
+    if(err){
+      console.log('ERROR IN DA HOUSE', err)
+    }else{
+      res.send(rows);
+      console.log("sucessfull bitch", rows)
+    }
+  })
 })
 
-router.post('/hash', (req, res, next) => {
-    const plainText = req.body.plainText;
-    const hash = crypto.createHash('md5').update(plainText).digest('hex')
-    res.json({
-        plainText,
-        hash
-    })
-})
 
-router.get("/h", (req, res, next) => {
-    res.render('index', {name: "ppp"})
-   });
+router.get('/:id', (req, res) => {
+    hb.query("SELECT * FROM mysampletable WHERE ID = ?", [req.params.id], (err, rows, fields) => {
+      if(err){
+        console.log('ERROR IN DA HOUSE', err)
+      }else{
+        res.send(rows);
+        console.log("sucessfull bitch", rows)
+      }
+    })
+  })
+
+
+// router.get("/time", (req, res, next) => {
+//   res.send({ time: new Date().toISOString() });
+// });
+
+// router.post("/hash", (req, res, next) => {
+//   const plainText = req.body.plainText;
+//   const hash = crypto.createHash("md5").update(plainText).digest("hex");
+//   res.json({
+//     plainText,
+//     hash,
+//   });
+// });
+
+// router.post("/people", (req, res, next) => {
+//     const id = req.body.id;
+//     const name = req.body.name;
+//     const hobbies = req.body.hobbies;
+
+//     // const 
+// })
+
+// router.get("/h", (req, res, next) => {
+//   res.render("index", { name: "ppp" });
+// });
 module.exports = router;
